69c400af68a5d7a30052b0c797b8bb75a7fb46cd
farah <farah.hayat1@outlook.com>
2022-10-09 17:56:39 +0100

    final

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "1612d60437a6244c05b11a0341b3beec"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBJHFlWz22vZfBaJ6zxmMnbyA_wnwnBsoM"
